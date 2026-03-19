d1aa47c7c468aa1f896870e6db3416396cc659cb
Firebase yuji <yuji.10473@gmail.com>
2026-03-19 17:46:20 +0000

    じゃあ念の為、.envに移行して

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,7 +1,7 @@
 export const firebaseConfig = {
   "projectId": "studio-3109699954-e195d",
   "appId": "1:1059483388102:web:c0d49ba1bee5b8118defc7",
-  "apiKey": "",
+  "apiKey": process.env.NEXT_PUBLIC_FIREBASE_API_KEY || "",
   "authDomain": "studio-3109699954-e195d.firebaseapp.com",
   "measurementId": "",
   "messagingSenderId": "1059483388102"
