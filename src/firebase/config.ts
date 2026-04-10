ec914cb38581ec9529ed90882d714f471af51c6f
Firebase yuji <yuji.10473@gmail.com>
2026-04-10 08:44:36 +0000

    実装して

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -36,12 +36,19 @@ const getAuthDomain = () => {
 
 /**
  * Firebase 設定オブジェクト
+ * 本番ドメイン "firebasejapan.com" の場合は、直接文字列として割り当てることで
+ * 確実にカスタムドメインでの認証を有効にします。
  */
+const productionDomain = "firebasejapan.com";
+const resolvedAuthDomain = (typeof window !== 'undefined' && window.location.hostname === productionDomain)
+  ? productionDomain
+  : getAuthDomain();
+
 export const firebaseConfig = {
   "projectId": "studio-3109699954-e195d",
   "appId": "1:1059483388102:web:c0d49ba1bee5b8118defc7",
   "apiKey": process.env.NEXT_PUBLIC_FIREBASE_API_KEY || "",
-  "authDomain": getAuthDomain(),
+  "authDomain": resolvedAuthDomain,
   "measurementId": "",
   "messagingSenderId": "1059483388102"
 };
